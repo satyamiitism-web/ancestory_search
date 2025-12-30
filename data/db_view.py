62e880aebf73defff67341a74dbdd37f5acd78cf
satyam2829 <satyamanand5@gmail.com>
2025-12-30 13:55:33 +0530

    :sparkles: Add serial number

--- data/db_view.py
+++ data/db_view.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+
 def render_database_view(collection):
     """
     Fetches all documents from the collection, cleans specific columns, 
@@ -11,12 +12,18 @@ def render_database_view(collection):
         # Fetch all data from the collection
         data = list(collection.find())
 
+
     if data:
         df = pd.DataFrame(data)
+
 
         # Drop '_id' and 'slug' columns if they exist
         columns_to_drop = ['_id', 'slug']
         df = df.drop(columns=columns_to_drop, errors='ignore')
+
+        # Add serial number column at the beginning
+        df.insert(0, 'S.No', range(1, len(df) + 1))
+
 
         # Display the dataframe
         st.dataframe(
